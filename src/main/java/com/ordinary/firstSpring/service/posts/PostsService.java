d6d55014f6b32e6555a59418715f9b3944db7927
0rdinary <ac980925@gmail.com>
2022-02-11 17:27:35 +0900

    게시글 추가, 수정, 삭제 기능 구현

--- src/main/java/com/ordinary/firstSpring/service/posts/PostsService.java
+++ src/main/java/com/ordinary/firstSpring/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.ordinary.firstSpring.service.posts;
 
 import com.ordinary.firstSpring.domain.posts.Posts;
 import com.ordinary.firstSpring.domain.posts.PostsRepository;
+import com.ordinary.firstSpring.web.dto.PostsListResponseDto;
 import com.ordinary.firstSpring.web.dto.PostsResponseDto;
 import com.ordinary.firstSpring.web.dto.PostsSaveRequestDto;
 import com.ordinary.firstSpring.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +38,20 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
